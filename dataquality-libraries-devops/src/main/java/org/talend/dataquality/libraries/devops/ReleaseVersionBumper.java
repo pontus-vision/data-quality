5227ad9278195c022f5fff4033d83e4096371cca
build-talend <build-talend@users.noreply.github.com>
2019-04-25 14:22:51 +0200

    chore(Jenkins): Bump to next dev version 6.4.1-SNAPSHOT (#649)

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -58,7 +58,7 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "6.4.0";
+    private static final String TARGET_VERSION = "6.4.1-SNAPSHOT";
 
     private static final String TARGET_DAIKON_VERSION = "0.31.3";
 
